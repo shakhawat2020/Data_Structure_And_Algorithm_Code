71792f2072f8f1c3f553970a0b60f9112bedb441
Shuvo <zaim.jest@gmail.com>
2026-01-12 23:55:24 +0600

    Stack_Using_Doubly_Linked_list.cpp

---
+++ Stack/Stack_Using_Doubly_Linked_list.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+        Node(int val)
+        {
+            this->val = val;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+int main()
+{
+    
+    return 0;
+}
